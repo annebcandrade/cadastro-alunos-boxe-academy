db35f815198f7b479bf3d969c07ce73726f1ffeb
Anne Beatriz Andrade <annebeatrizandrade@MacBook-Air-de-Anne.local>
2024-04-02 18:46:35 -0300

    system-ready-front-conected-backend

--- src/containers/ViewRegister/index.jsx
+++ src/containers/ViewRegister/index.jsx
@@ -1,13 +1,115 @@
-import { Container } from "./styles"
+import { Button, Container, ContainerThree, ContainerTwo, InputCpf, ListFive } from "./styles"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import api from '../../services/api'
 
 function ViewRegister() {
 
+    const [alunos, setAlunos] = useState([]);
+    const [ordenado, setOrdenado] = useState(false);
+    const [cpf, setCpf] = useState('');
+    const [aluno, setAluno] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
+    useEffect(() => {
+        async function fetchAlunos() {
+            try {
+                const response = await api.get('/');
+                const alunosOrdenados = response.data.retorno.sort((a, b) => (a.nome > b.nome) ? 1 : -1);
 
-    return(
+                const primeirosCincoAlunos = alunosOrdenados.slice(0, 5);
+                setAlunos(primeirosCincoAlunos);
+            } catch (error) {
+                console.error('Erro ao obter os alunos:', error);
+            }
+        }
+
+        fetchAlunos();
+    }, []);
+
+    const handleOrdenarAlunos = () => {
+        if (!ordenado) {
+            const alunosOrdenados = [...alunos].sort((a, b) => (a.nome > b.nome) ? 1 : -1);
+            setAlunos(alunosOrdenados);
+        } else {
+            const alunosDesordenados = [...alunos].sort((a, b) => (a.nome > b.nome) ? -1 : 1);
+            setAlunos(alunosDesordenados);
+        }
+        setOrdenado(!ordenado);
+    };
+
+    const handleBuscarAluno = async () => {
+        try {
+            const response = await api.get(`/${cpf}`);
+            if (response.status === 200) {
+                const alunoData = response.data.retorno;
+                setErrorMessage('Aluno Cadastrado na Boxe Academy. Confira os dados cadastrados logo abaixo: ')
+               
+                if (alunoData) {
+                  setAluno(alunoData); 
+                } else {
+                  setAluno(null);
+                  setErrorMessage('Aluno não cadastrado na Boxe Academy');
+                }
+              } else {
+                setAluno(null);
+                setErrorMessage('Aluno não cadastrado na Boxe Axademy. Por favor, tente novamente.');
+              }
+            } catch (error) {
+              console.error('Aluno não cadastrado na Boxe Axademy.', error);
+              setAluno(null);
+              setErrorMessage('Aluno não cadastrado na Boxe Axademy. Por favor, tente novamente.');
+            }
+          };
+
+    return (
         <Container>
             <h1>Ver Cadastros</h1>
-            <p>Confira a lista de alunos cadastrados na Boxe Academy</p>
+            <p>Confira a lista de alunos cadastrados na Boxe Academy em Ordem de
+                inscrição:</p>
+
+            <Button onClick={handleOrdenarAlunos}>{ordenado ? 'Ordem Alfabética' : 'Alunos mais Recentes'}</Button>
+
+
+            <Container>
+                <ListFive>
+                    {alunos.map((aluno) => (
+                        <li key={aluno.cpf}>Nome: {aluno.nome} {aluno.sobrenome} | CPf: {aluno.cpf}</li>
+                    ))}
+                </ListFive>
+            </Container>
+
+            <h1>Para procurar os dados de um aluno digite o CPF:</h1>
+
+            <ContainerTwo>
+                <InputCpf value={cpf}
+                    onChange={(e) => setCpf(e.target.value)}
+                    placeholder="Digite o cpf do aluno" />
+                <Button onClick={handleBuscarAluno}>Procurar</Button>
+            </ContainerTwo>
+
+            {errorMessage && <p>{errorMessage}</p>}
+      {aluno && (
+        <ContainerThree>
+          <h2>Dados do Aluno</h2>
+          <p>Nome: {aluno.nome}</p>
+          <p>Sobrenome: {aluno.sobrenome}</p>
+          <p>Data de Nascimento: {aluno.dataNascimento}</p>
+          <p>CPF: {aluno.cpf}</p>
+          <p>Cidade: {aluno.cidade}</p>
+          <p>Logradouro: {aluno.logradouro}</p>
+          <p>Bairro: {aluno.bairro}</p>
+          <p>UF: {aluno.uf}</p>
+          <p>Email: {aluno.email}</p>
+          <p>Peso: {aluno.peso}</p>
+          <p>Altura: {aluno.altura}</p>
+          <p>Complemento: {aluno.complemento}</p>
+          <p>Número: {aluno.numero}</p>
+        </ContainerThree>
+      )}
+
+
+
         </Container>
     )
 }
--- src/containers/ViewRegister/styles.js
+++ src/containers/ViewRegister/styles.js
@@ -21,3 +21,94 @@ export const Container = styled.div`
 
 
 `
+
+export const ContainerTwo = styled.div`
+  display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+
+    h1 {
+        margin-top: 120px;
+        color: #fff;
+        font-size: 22;
+    }
+
+    p{ 
+        margin-top: 20px;
+        color: #fff;
+    }
+
+
+
+
+`
+
+export const ContainerThree = styled.div`
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0px;
+
+    h1 {
+        margin-top: 120px;
+        color: #fff;
+        font-size: 22;
+    }
+
+    h2 {
+        margin-top: 120px;
+        color: #fff;
+        font-size: 20;
+    }
+
+    p{ 
+        color: #fff;
+    }
+
+
+
+
+`
+
+
+export const ListFive = styled.ul`
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    li {
+        margin-top: 12px;
+        color: #fff;
+        font-size: 22;
+    }
+
+`
+
+export const Button = styled.button`
+    background-color: #8b0000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    height: 40px;
+    width: 100px;
+    border-radius: 10px;
+    margin-top: 20px;
+
+`
+
+export const InputCpf = styled.input`
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    width: 300px;
+    border-radius: 10px;
+    margin-top: 20px;
+
+`
